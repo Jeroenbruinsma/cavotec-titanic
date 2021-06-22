b6f68d9250d3bb6bf4b2864abe262b626d1a8250
Jeroenbruinsma <jeroenbruinsma@gmail.com>
2021-06-22 21:19:54 +0200

    implement people get endpoint

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
 const db = require("./models");
+const passengers = require("./models").passengers;
 
 const app = express();
-const port = 6000;
+const port = 5000;
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Titanic Engine running!");
-});
+  });
 
-app.get("/people", (req, res) => {
-  res.send("Titanic Engine running!");
+app.get("/people", async (req, res) => {
+  const offset = req.query.offset || 0
+  const limit = req.query.limit || 30
+  try{
+    const ppl = await passengers.findAll({ offset, limit })
+    res.json(ppl)
+  }
+  catch(err){
+    console.log(err)
+    res.status(500)
+  }
 });
 
 app.listen(port, () => {
